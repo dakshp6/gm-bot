fa3b9241a6424299dd04692cb35f2a8df05ca60a
Daksh Patel <dakshpatel6@gmail.com>
2020-01-01 03:49:56 -0500

    fixed error with reply function

--- bot.py
+++ bot.py
@@ -10,14 +10,13 @@ def webhook():
 
     msg = request.get_json()
 
-    if '@bills' in msg['text'].lower():
+    if '@bills' in (msg['text'].lower()):
         reply('No bills right now!!')
 
     return "ok", 200
 
 
-botid = os.getenv('GROUPME_BOT_ID')
-
-
 def reply(msg):
+    
+    botid = os.getenv('GROUPME_BOT_ID')
     requests.post('https://api.groupme.com/v3/bots/post?bot_id='+botid+'&text='+msg)
