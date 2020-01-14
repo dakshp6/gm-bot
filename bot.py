54eda0c5cbe24f3637f97437f48490eb7b1a5a59
Daksh Patel <dakshpatel6@gmail.com>
2020-01-14 01:06:53 -0500

    fixed issues

--- bot.py
+++ bot.py
@@ -15,40 +15,36 @@ def webhook():
         #lists out db list
         db = db_connect()
         reply(show_list(db))
-
+    
     elif '/add' in msg['text'].lower():
         #collect info into list and add to db
-
+    
         ls = parse_text("/add ",msg['text'].lower())
         db = db_connect()
-
+    
         for food in ls:
             add_list(db,food)
-
-        reply("ok")
-
+    
+        reply('ok')
+    
     elif '/remove' in msg['text'].lower():
         #collect info into list and remove from db
         ls = parse_text("/remove ", msg['text'].lower())
         db = db_connect()
         for food in ls:
             del_list(db,food)
-
-        reply("ok")
-
+    
+        reply('ok')
+    
     return "ok", 200
-
 
 def reply(msg):
     requests.post('https://api.groupme.com/v3/bots/post?bot_id='+os.getenv('GROUPME_BOT_ID')+'&text='+msg)
 
-
 def parse_text(method,text):
     text = text.replace(method, "")
-
     parsed_list = text.split(',')
     fl = []
-
     for things in parsed_list:
         things = things.strip(' ')
         fl.append(things)
@@ -63,15 +59,12 @@ def db_connect():
 def add_list(db,food):
     db.grocerylist.insert_one({'item' : food})
 
-
 def del_list(db,food):
     db.grocerylist.delete_one({'item' : food})
 
 def show_list(db):
     result = db.grocerylist.find()
-    txt = ""
-
+    txt = ''
     for docs in result:
         txt += docs['item'] + '\n'
-
     return txt
